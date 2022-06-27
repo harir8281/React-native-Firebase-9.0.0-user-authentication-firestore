8f0fd171dc8720babb30d5129b9567e6094817cc
Harikrishnan.r <harir3737@gmail.com>
2022-06-27 23:16:53 +0530

    commit before navigation

--- App.js
+++ App.js
@@ -1,26 +1,59 @@
-import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button,Alert} from 'react-native';
 import React, {useState} from 'react';
 
 import {authentication} from './Firebase/firebase-config';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from 'firebase/auth';
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
- function registerUser()
- {
-  createUserWithEmailAndPassword(authentication,email,password)
-  .then((re)=>{
-    console.log(re)
-  })
-  .catch((re)=>{
-    console.log(re)
-  })
- }
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
 
-  
+  function registerUser() {
+    createUserWithEmailAndPassword(authentication, email, password)
+      .then(re => {
+        console.log(re);
+        setIsSignedIn(true);
+        Alert.alert("user registered")
+      })
+      .catch(re => {
+        console.log(re);
+      });
+  }
+
+  function signInUser() {
+    signInWithEmailAndPassword(authentication,signInEmail,signInPassword)
+    .then((re)=>{
+      setIsSignedIn(true)
+      Alert.alert("signin successfully")
+    })
+    .catch((re)=>{
+      console.log(re)
+      Alert.alert("username or password incorrect")
+    })
+  }
+
+  function signOutUser()
+  {
+    signOut(authentication)
+    .then((re)=>{
+      setIsSignedIn(false)
+      Alert.alert("Signout successfully")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -30,7 +63,7 @@ const App = () => {
           setEmail(text);
         }}
       />
-      
+
       <TextInput
         placeholder="password"
         value={password}
@@ -39,6 +72,28 @@ const App = () => {
         }}
       />
       <Button title="register" onPress={registerUser} />
+      <View style={styles.underview}>
+        <TextInput
+          placeholder="username"
+          value={signInEmail}
+          onChangeText={text => {
+            setSignInEmail(text);
+          }}
+        />
+
+        <TextInput
+          placeholder="password"
+          value={signInPassword}
+          onChangeText={text => {
+            setSignInPassword(text);
+          }}
+        />
+        {isSignedIn ?
+        <Button title="sign out" onPress={signOutUser} />
+        :
+        <Button title="sign in" onPress={signInUser} />
+        }
+      </View>
     </View>
   );
 };
@@ -49,5 +104,8 @@ const styles = StyleSheet.create({
   container: {
     padding: 30,
     flex: 1,
+  },
+  underview: {
+    marginVertical: 50,
   },
 });
