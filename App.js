dacb8873533278db2c2fabe69fcdafc9725e7623
Harikrishnan.r <harir3737@gmail.com>
2022-06-29 11:56:28 +0530

    firstore cities added

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View, TextInput, Button,Alert} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
 import React, {useState} from 'react';
 
 import {authentication} from './Firebase/firebase-config';
@@ -6,115 +6,171 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+} from 'firebase/firestore/lite';
+
+import {db} from './Firebase/firebase-config';
+
 const App = () => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  //user authentication code
+  // const [isSignedIn, setIsSignedIn] = useState(false);
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  // const [signInEmail, setSignInEmail] = useState('');
+  // const [signInPassword, setSignInPassword] = useState('');
+
+  //usestate for firestore
+  const [cityName, setCityName] = useState('');
+  const [stateName, setStateName] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const [signInEmail, setSignInEmail] = useState('');
-  const [signInPassword, setSignInPassword] = useState('');
+  // function registerUser() {
+  //   createUserWithEmailAndPassword(authentication, email, password)
+  //     .then(re => {
+  //       console.log(re);
+  //       setIsSignedIn(true);
+  //       Alert.alert("user registered")
+  //     })
+  //     .catch(re => {
+  //       console.log(re);
+  //     });
+  // }
 
-  function registerUser() {
-    createUserWithEmailAndPassword(authentication, email, password)
-      .then(re => {
-        console.log(re);
-        setIsSignedIn(true);
-        Alert.alert("user registered")
-      })
-      .catch(re => {
-        console.log(re);
-      });
+  // function signInUser() {
+  //   signInWithEmailAndPassword(authentication,signInEmail,signInPassword)
+  //   .then((re)=>{
+  //     setIsSignedIn(true)
+  //     Alert.alert("signin successfully")
+  //   })
+  //   .catch((re)=>{
+  //     console.log(re)
+  //     Alert.alert("username or password incorrect")
+  //   })
+  // }
+
+  // function signOutUser()
+  // {
+  //   signOut(authentication)
+  //   .then((re)=>{
+  //     setIsSignedIn(false)
+  //     Alert.alert("Signout successfully")
+  //     setSignInEmail('')
+  //     setSignInPassword('')
+
+  //   })
+  //   .catch((err)=>{
+  //     console.log(err)
+  //   })
+  // }
+
+  //firstore code
+
+  async function getData() {
+    const citiesCol = collection(db, 'cities');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc => doc.data());
+    console.log(cityList);
   }
 
-  function signInUser() {
-    signInWithEmailAndPassword(authentication,signInEmail,signInPassword)
-    .then((re)=>{
-      setIsSignedIn(true)
-      Alert.alert("signin successfully")
-    })
-    .catch((re)=>{
-      console.log(re)
-      Alert.alert("username or password incorrect")
-    })
+  async function setData() {
+    // Add a new document in collection "cities"
+    await setDoc(doc(db, 'cities', email), {
+      city_name: cityName,
+      state: stateName,
+    });
   }
-
-  function signOutUser()
-  {
-    signOut(authentication)
-    .then((re)=>{
-      setIsSignedIn(false)
-      Alert.alert("Signout successfully")
-      setSignInEmail('')
-      setSignInPassword('')
-
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
-
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        placeholder="username"
-        value={email}
-        onChangeText={text => {
-          setEmail(text);
-        }}
-      />
-
-      <TextInput
-        placeholder="password"
-        value={password}
-        onChangeText={text => {
-          setPassword(text);
-        }}
-      />
-      <Button title="register" onPress={registerUser} />
-      <View style={styles.underview}>
+    <View style={styles.firestoreView}>
+      <Button title="Get data" onPress={getData} color="black" />
+      <View style={styles.inputView}>
         <TextInput
-          placeholder="username"
-          value={signInEmail}
-          onChangeText={text => {
-            setSignInEmail(text);
-          }}
+          placeholder="City name"
+          onChangeText={text => setCityName(text)}
         />
-
         <TextInput
-          placeholder="password"
-          value={signInPassword}
-          onChangeText={text => {
-            setSignInPassword(text);
-          }}
+          placeholder="State"
+          onChangeText={text => setStateName(text)}
         />
-        {isSignedIn ?
-        <Button title="sign out" onPress={signOutUser} />
-        :
-        <Button title="sign in" onPress={signInUser} />
-        }
+        <TextInput placeholder="Email" onChangeText={text => setEmail(text)} />
       </View>
-      <View style={styles.thirdview} >
-      </View>
+      <Button title="set data" onPress={setData} color="#556b2f" />
     </View>
+    // <View style={styles.container}>
+    //   <TextInput
+    //     placeholder="username"
+    //     value={email}
+    //     onChangeText={text => {
+    //       setEmail(text);
+    //     }}
+    //   />
+
+    //   <TextInput
+    //     placeholder="password"
+    //     value={password}
+    //     onChangeText={text => {
+    //       setPassword(text);
+    //     }}
+    //   />
+    //   <Button title="register" onPress={registerUser} />
+    //   <View style={styles.underview}>
+    //     <TextInput
+    //       placeholder="username"
+    //       value={signInEmail}
+    //       onChangeText={text => {
+    //         setSignInEmail(text);
+    //       }}
+    //     />
+
+    //     <TextInput
+    //       placeholder="password"
+    //       value={signInPassword}
+    //       onChangeText={text => {
+    //         setSignInPassword(text);
+    //       }}
+    //     />
+    //     {isSignedIn ?
+    //     <Button title="sign out" onPress={signOutUser} />
+    //     :
+    //     <Button title="sign in" onPress={signInUser} />
+    //     }
+    //   </View>
+    //   <View style={styles.thirdview} >
+    //   </View>
+    // </View>
   );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
+  // container: {
+  //   padding: 20,
+  //   flex: 1,
+  // },
+  // underview: {
+  //   marginVertical: 25,
+  // },
+  // thirdview:{
+  //   backgroundColor:"cyan",
+  //   flex:1
+  // }
+
+  firestoreView: {
+    padding: 30,
     flex: 1,
+    backgroundColor: '#f0e68c',
   },
-  underview: {
-    marginVertical: 25,
+  inputView: {
+    padding: 10,
   },
-  thirdview:{
-    backgroundColor:"cyan",
-    flex:1
-  }
 });
